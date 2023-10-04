f6a1c30c536b70bec06ce52384e0a009df7b42fc
roniskywalker <ankanhazraeducation@gmail.com>
2023-10-05 00:16:08 +0530

    Created forecast data

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from prophet import Prophet
 from prophet.plot import plot_plotly
 from plotly import graph_objs as go
 
-START = "2006-01-01"
+START = "2015-01-01"
 TODAY = date.today().strftime("%y-%m-%d")
 
-st.title("Stock Prediction App")
+st.title("Predict")
 
-stocks = ("AAPL", "GOOG", "MSFT")
+stocks = ('AAPL', 'GOOG', 'MSFT')
 selected_stock = st.selectbox("Select dataset for prediction", stocks)
 
 n_years = st.slider("Years of prediction:", 1, 3)
@@ -20,12 +20,10 @@ period = n_years * 365
 @st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
-    data.reset_index(inplace = True)
+    data.reset_index(inplace=True)
     return data
 
-data_load_state = st.text("Load data...")
 data = load_data(selected_stock)
-data_load_state.text("Loading data...done!")
 
 st.subheader('Raw Data:')
 st.write(data.tail())
@@ -38,3 +36,14 @@ def plot_raw_data():
     st.plotly_chart(fig)
 
 plot_raw_data()
+
+df_train = data[['Date', "Close"]]
+df_train = df_train.rename(columns={"Date":"ds", "Close":"y"})
+
+m = Prophet()
+m.fit(df_train)
+future = m.make_future_dataframe(periods=period)
+forecast = m.predict(future)
+
+st.subheader('Forecast Data:')
+st.write(forecast.tail())
