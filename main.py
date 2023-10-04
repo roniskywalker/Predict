e55b8d4d853678a6c22d3df5246c9f752da01710
roniskywalker <ankanhazraeducation@gmail.com>
2023-10-04 22:59:13 +0530

    Added date, stocks and years

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import streamlit as st
 from datetime import date
 
 import yfinance as yf
-from fbprophet import Prophet
-from fbprophet.plot import plot_plotly
+from prophet import Prophet
+from prophet.plot import plot_plotly
 from plotly import graph_objs as go
+
+START = "2006-01-01"
+TODAY = date.today().strftime("%y-%m-%d")
+
+st.title("Stock Prediction App")
+
+stocks = ("AAPL", "GOOG", "MSFT")
+selected_stocks = st.selectbox("Select dataset for prediction", stocks)
+
+n_years = st.slider("Years of prediction:", 1, 3)
+period = n_years * 365
