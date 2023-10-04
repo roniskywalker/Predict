40e4581890c725cc4b360307c6c0d16a9e0ca69b
roniskywalker <ankanhazraeducation@gmail.com>
2023-10-04 23:40:11 +0530

    Plotted data

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ selected_stock = st.selectbox("Select dataset for prediction", stocks)
 n_years = st.slider("Years of prediction:", 1, 3)
 period = n_years * 365
 
+@st.cache_data
 def load_data(ticker):
     data = yf.download(ticker, START, TODAY)
     data.reset_index(inplace = True)
@@ -25,3 +26,15 @@ def load_data(ticker):
 data_load_state = st.text("Load data...")
 data = load_data(selected_stock)
 data_load_state.text("Loading data...done!")
+
+st.subheader('Raw Data:')
+st.write(data.tail())
+
+def plot_raw_data():
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Open'], name = 'stock_open'))
+    fig.add_trace(go.Scatter(x=data['Date'], y=data['Close'], name = 'stock_close'))
+    fig.layout.update(title_text="Time Series Data", xaxis_rangeslider_visible=True)
+    st.plotly_chart(fig)
+
+plot_raw_data()
